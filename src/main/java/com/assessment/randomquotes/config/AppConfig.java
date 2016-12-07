a08147fd4ed1f971268d2aab95e5abf3da1afde9
Matias <matiasterracciano@gmail.com>
2016-12-07 01:05:51 -0300

    Added simple example of srpingmvc WIP

---
+++ src/main/java/com/assessment/randomquotes/config/AppConfig.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.assessment.randomquotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author matias.terracciano
+ *
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.assessment.randomquotes")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+}
