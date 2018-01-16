44dd372ca72995634cb985d8f31378c58313411a
Matias <matiasterracciano@gmail.com>
2018-01-16 01:18:55 -0300

    missing file

---
+++ src/main/java/com/assessment/randomquotes/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.assessment.randomquotes.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
