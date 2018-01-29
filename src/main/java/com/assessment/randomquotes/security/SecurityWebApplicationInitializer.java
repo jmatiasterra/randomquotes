0bc9a106da3be4237e819e92006803c055748705
Matias <matiasterracciano@gmail.com>
2018-01-29 00:40:05 -0300

    Added basic configuration to use OAuth2. I need to add some testing for this. WIP

--- src/main/java/com/assessment/randomquotes/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.assessment.randomquotes.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
